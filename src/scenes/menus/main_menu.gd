742e4c19b289be282a7980ec9605dfabef7a0693
Yannick G. N. Mikkelsen <yamik22@student.sdu.dk>
2024-08-17 14:42:23 +0200

    Removed tab

--- src/scenes/menus/main_menu.gd
+++ src/scenes/menus/main_menu.gd
@@ -4,7 +4,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/Startbutton.grab_focus()	
-	
+
 func _on_startbutton_pressed():
 	get_tree().change_scene_to_file("res://src/scenes/game-scenes/test_scene.tscn")
 
