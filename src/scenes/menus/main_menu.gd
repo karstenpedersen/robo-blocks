fbb194e98269207b3f70fb5866ef80c27a137f12
Yannick G. N. Mikkelsen <yamik22@student.sdu.dk>
2024-08-17 14:28:26 +0200

    Deleted Unused Code

--- src/scenes/menus/main_menu.gd
+++ src/scenes/menus/main_menu.gd
@@ -3,14 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/Startbutton.grab_focus()
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
-	
+	$VBoxContainer/Startbutton.grab_focus()	
 	
 func _on_startbutton_pressed():
 	get_tree().change_scene_to_file("res://src/scenes/game-scenes/test_scene.tscn")
