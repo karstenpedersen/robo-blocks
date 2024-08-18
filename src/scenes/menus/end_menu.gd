bd4a8d93c017feffc0b83b4e70a8c3f40c2497fe
Yannick <yamik22@student.sdu.dk>
2024-08-18 21:16:45 +0200

    Added end scene layout

---
+++ src/scenes/menus/end_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var main = $"../"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$CenterContainer/VBoxContainer/NewRun.grab_focus()
+
+	
+
+
+func _on_new_run_pressed() -> void:
+	main.alive()
+	get_tree().change_scene_to_file("res://src/scenes/game-scenes/test_scene.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_to_main_menu_pressed() -> void:
+	main.alive()
+	get_tree().change_scene_to_file("res://src/scenes/menus/main_menu.tscn")
+	
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
