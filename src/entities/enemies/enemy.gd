4f4148a1e9c9c42e72000a5923c5e8df16729c05
Tobias Samsøe Sørensen <tobi8380@gmail.com>
2024-08-20 11:32:29 +0200

    Add targeting turret component and update enemy to use it

--- src/entities/enemies/enemy.gd
+++ src/entities/enemies/enemy.gd
@@ -8,7 +8,6 @@ enum EnemyMode {
 
 @export var accel = 4000
 @export var max_speed = 6
-@export var rotation_speed = 2
 
 # Distance at which the enemy stops moving towards player
 @export var follow_distance = 8
@@ -19,10 +18,14 @@ enum EnemyMode {
 @export var target: Node3D
 
 var mode: EnemyMode
+var turret: TargetingTurret
+#@onready var turret = $TargetingTurret
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	lock_rotation = true
+	turret = $TargetingTurret
+	turret.set_target(target)
 	set_mode(EnemyMode.IDLE)
 
 
@@ -34,17 +37,16 @@ func _physics_process(delta):
 func movement(delta):
 	match mode:
 		EnemyMode.IDLE:
-			rotation.y += delta * rotation_speed
+			pass
 		EnemyMode.WANDER:
 			apply_force(transform.basis.z.normalized() * accel * delta)
 		EnemyMode.FOLLOW_TARGET:
-			var direction = position.direction_to(target.position)
-			var target_angle = atan2(direction.x, direction.z)
-			rotation.y = rotate_toward(rotation.y, target_angle, rotation_speed * delta)
-			
-			
+			#var direction = position.direction_to(target.position)
+			#var target_angle = atan2(direction.x, direction.z)
+			#rotation.y = rotate_toward(rotation.y, target_angle, rotation_speed * delta)
+
 			var distance = position.distance_to(target.position)
-			var angle_diff = abs(rotation.y - target_angle)
+			var angle_diff = turret.get_angle_diff()
 			if distance > follow_distance and angle_diff < follow_angle_threshold:
 				apply_force(transform.basis.z.normalized() * accel * delta)
 
@@ -73,7 +75,6 @@ func _on_idle_timer_timeout() -> void:
 	if mode == EnemyMode.IDLE:
 		set_mode(EnemyMode.WANDER)
 
-
 func _on_wander_timer_timeout() -> void:
 	if mode == EnemyMode.WANDER:
 		set_mode(EnemyMode.IDLE)
@@ -81,6 +82,13 @@ func _on_wander_timer_timeout() -> void:
 func set_mode(m: EnemyMode):
 	if m == EnemyMode.IDLE:
 		$IdleTimer.start(randf_range(0.5, 2))
+		if randi() % 2 == 0:
+			turret.set_mode_spin_clockwise()
+		else:
+			turret.set_mode_spin_counterclockwise()
 	elif m == EnemyMode.WANDER:
 		$WanderTimer.start(randf_range(0.5, 2))
+		turret.set_mode_idle()
+	elif m == EnemyMode.FOLLOW_TARGET:
+		turret.set_mode_targeting()
 	mode = m
